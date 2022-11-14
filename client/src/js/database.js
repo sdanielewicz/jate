9795bf3e0913204e96d131e6538ffe0fa5378f55
sdanielewicz <sdanielewicz@gmail.com>
2022-11-14 12:51:21 -0800

    started todos

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,40 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('Put to the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  // const request = store.put({ id: id, todo: content });
+try{  
+  const result = await request;
+  console.log('data saved to the database', result);
+}
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+catch{
+  console.error('putDb not implemented');
+}
+}
 
+
+export const getDb = async () => {
+  console.log('GET from the database');
+
+  const jateDb = await openDB('jate', 1);
+
+  const tx = jateDb.transaction('jate', 'readonly');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.getAll();
+try{
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+}
+catch{
+  console.error('getDb not implemented');
+}
+}
 initdb();
